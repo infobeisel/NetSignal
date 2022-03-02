454e00fdedba54bff43f5523d0a498ccfb390f60
Philipp Beisel <infobeisel@gmail.com>
2022-03-02 10:17:36 +0100

    add highscore indices

--- FlyByIslandMultiplayerShared/ReliableSignalIndices.cs
+++ FlyByIslandMultiplayerShared/ReliableSignalIndices.cs
@@ -7,5 +7,9 @@ namespace NetSignal2Unity
         public const int TIMESTAMP_1 = 3;
         public const int TERRAIN_ID = 4;
         public const int TRACK_ID = 5;
+        public const int HIGHSCORE_MS = 6;
+        public const int HIGHSCORE_RINGS = 7;
+        public const int HIGHSCORE_LOOT = 8;
+        
     }
 }
